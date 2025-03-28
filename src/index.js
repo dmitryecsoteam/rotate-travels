7af7e8125fc91d95743eff5ff18bc3bd321cea6e
Dmitry Kiselev <DKiselev@inno.tech>
2025-03-28 17:12:25 +0300

    console log test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const rotateTravels = require('./service/rotateTravels');
-const mongo = require('./server/mongo');
-const moment = require('moment');
+const rotateTravels = require("./service/rotateTravels");
+const mongo = require("./server/mongo");
+const moment = require("moment");
 
 const run = async () => {
+  // Connect to mongo DB
+  const db = await mongo.connect();
 
-    // Connect to mongo DB
-    const db = await mongo.connect();
+  await rotateTravels(moment(), db);
 
-    await rotateTravels(moment(), db);
-
-    await mongo.close();
-}
+  await mongo.close();
+};
 
 run();
+
+console, log("test");
